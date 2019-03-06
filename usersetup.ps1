70091d4786c318dec696df25547f06ebf46210c4
Esteban <eariasn@us.ibm.com>
2019-03-06 09:39:15 -0500

    Update usersetup.ps1

--- usersetup.ps1
+++ usersetup.ps1
@@ -3,7 +3,7 @@ secedit /export /cfg c:\secpol.cfg
 (gc C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
 secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
 rm -force c:\secpol.cfg -confirm:$false
-([adsi]"WinNT://$env:computername/Administrator").SetPassword('welcome2ibm')
+([adsi]"WinNT://$env:computername/Administrator").SetPassword('welcome2ibm%%%')
 winrm quickconfig -q
 winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="1024"}'
 winrm set winrm/config '@{MaxTimeoutms="1800000"}'
