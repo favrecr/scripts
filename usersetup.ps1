90a82058b341e763aa348c1ec1d5dc550146955b
Esteban <eariasn@us.ibm.com>
2019-03-11 13:46:41 -0400

    Update usersetup.ps1

--- usersetup.ps1
+++ usersetup.ps1
@@ -1,5 +1,8 @@
 #ps1_sysnative
 Enable-WindowsOptionalFeature –Online –FeatureName MultiPathIO
+Enable-MSDSMAutomaticClaim -BusType iSCSI
+Set-MSDSMGlobalDefaultLoadBalancePolicy -Policy RR
+Set-MPIOSetting -NewDiskTimeout 60
 secedit /export /cfg c:\secpol.cfg
 (gc C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
 secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
