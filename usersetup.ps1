3b967a912a2c469ecfdb26285d14c60c7d5a429e
Esteban <eariasn@us.ibm.com>
2019-03-11 13:47:33 -0400

    Update usersetup.ps1

--- usersetup.ps1
+++ usersetup.ps1
@@ -3,8 +3,3 @@ Enable-WindowsOptionalFeature –Online –FeatureName MultiPathIO
 Enable-MSDSMAutomaticClaim -BusType iSCSI
 Set-MSDSMGlobalDefaultLoadBalancePolicy -Policy RR
 Set-MPIOSetting -NewDiskTimeout 60
-secedit /export /cfg c:\secpol.cfg
-(gc C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
-secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
-rm -force c:\secpol.cfg -confirm:$false
-([adsi]"WinNT://$env:computername/Administrator").SetPassword('welcome2ibm%%%')
