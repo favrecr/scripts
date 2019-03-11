ab25d0ff702e41d89004d8441445b9b8f460e1dd
Esteban <eariasn@us.ibm.com>
2019-03-11 14:36:20 -0400

    Update usersetup.ps1

--- usersetup.ps1
+++ usersetup.ps1
@@ -1,5 +1,5 @@
 #ps1_sysnative
-Enable-WindowsOptionalFeature –Online –FeatureName MultiPathIO
+Enable-WindowsOptionalFeature -Online -FeatureName MultiPathIO
 Enable-MSDSMAutomaticClaim -BusType iSCSI
 Set-MSDSMGlobalDefaultLoadBalancePolicy -Policy RR
 Set-MPIOSetting -NewDiskTimeout 60
